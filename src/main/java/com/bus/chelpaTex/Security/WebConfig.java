32c5a36e21f3dd7c622648fa63f943aa4afcabfa
Andres9601 <sandresi9601@gmail.com>
2023-01-21 23:59:27 -0500

    Create WebConfig.java

---
+++ src/main/java/com/bus/chelpaTex/Security/WebConfig.java
@@ -0,0 +1,24 @@
+package com.bus.chelpaTex.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+	  @Override
+	  public void addCorsMappings(CorsRegistry registry) {
+
+	    registry.addMapping("./*")
+	      .allowedOrigins("*")
+	      .allowedMethods("GET", "POST" , "DELETE" , "PUT" , "HEAD" , "PATCH")
+	      .allowedHeaders("header1", "header2", "header3")
+	      .exposedHeaders("header1", "header2")
+	      .allowCredentials(true).maxAge(3600);
+
+	    // Add more mappings...
+	  }
+	}

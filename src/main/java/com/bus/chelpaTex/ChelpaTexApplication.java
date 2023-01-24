b532bf3715a98a6e275b1e4d1bbcf868c3e4c797
Andres9601 <sandresi9601@gmail.com>
2023-01-24 14:18:24 -0500

    EnableAsync

--- src/main/java/com/bus/chelpaTex/ChelpaTexApplication.java
+++ src/main/java/com/bus/chelpaTex/ChelpaTexApplication.java
@@ -3,7 +3,9 @@ package com.bus.chelpaTex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.bus.chelpaTex.Entity","com.bus.chelpaTex.Controller","com.bus.chelpaTex.DTO","com.bus.chelpaTex.Repo","com.bus.chelpaTex.Service","com.bus.chelpaTex.ServiceImpl","com.bus.chelpaTex.Security"})
 public class ChelpaTexApplication {

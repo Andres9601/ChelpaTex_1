2825fbd09da852982cf8fbd808b9c58f9a367747
Andres9601 <sandresi9601@gmail.com>
2023-02-20 21:06:13 -0500

    Item

--- src/main/java/com/bus/chelpaTex/DTO/ItemDTO.java
+++ src/main/java/com/bus/chelpaTex/DTO/ItemDTO.java
@@ -27,7 +27,96 @@ public class ItemDTO implements Serializable{
 	private BigDecimal precioUnidad;
 	
 	private BigDecimal cantidadMinima;
+	
+	private String nombre;
+	
+	private String imagen;
+	
+	private String origen;
+	
+	private String fabricante; 
+	
+	private String referencia;
+	
+	private String uso;
+	
+	private String segmento_categoria;
+	
+	private String base;
+	
+	private String ligamento;
+	
+	private String codigo_acabado;
+	
+	private BigDecimal anchoTotal;
+	
+	private BigDecimal pesoGramaje; 
+	
+	private BigDecimal peso; 
+	
+	private String color; 
+	
+	private String composicion;
+	
+	private String tipoTejido;
+	
+	private String diseno;
+	
+	private String ubicacion;
+	
+	private String tamano;
+	
+	private String tipoMaterial;
+	
+	private String tipoPrenda;
+	
+	private String maquinasEspeciales;
+	
+	private BigDecimal cantidadPrenda; 
+	
+	private String objetivo; 
+	
+	private String usoSugerido;
+	
+	private String tipoTintura;
+	
+	private String ecologico;
+	
+	private String impactoSocial; 
+	
+	private String normaCertificacion;
+	
+	private String anchoUtil;
+	
+	private String longitud;
+	
+	private String calibreGrosor; 
+	
+	private String aseguramiento; 
+	
+	private String forma;
+	
+	private BigDecimal numeroLigas;
+	
+	private String textura;
+	
+	private String encogimiento;
+	
+	private String unidad;	
 			
 	private Boolean activo;
 
+	public ItemDTO(Long idItem, String tipoItem, String idTrazabilidad, BigDecimal precioUnidad,
+			BigDecimal cantidadMinima, Boolean activo) {
+		super();
+		this.idItem = idItem;
+		this.tipoItem = tipoItem;
+		this.idTrazabilidad = idTrazabilidad;
+		this.precioUnidad = precioUnidad;
+		this.cantidadMinima = cantidadMinima;
+		this.activo = activo;
+	}
+
+	
+	
 }
--- src/main/java/com/bus/chelpaTex/Entity/Item.java
+++ src/main/java/com/bus/chelpaTex/Entity/Item.java
@@ -48,9 +48,135 @@ import lombok.AllArgsConstructor;
 		
 		@Column(name = "cantidad_minima")
 		private BigDecimal cantidadMinima;
-				
+		
+		
+		@Column(name = "nombre")
+		private String nombre;
+		
+		@Column(name = "imagen")
+		private String imagen;
+		
+		@Column(name = "origen")
+		private String origen;
+		
+		@Column(name = "fabricante")
+		private String fabricante;
+		
+		@Column(name = "referencia")
+		private String referencia;
+		
+		@Column(name = "uso")
+		private String uso;
+		
+		@Column(name = "segmento_categoria")
+		private String segmentoCategoria;
+		
+		@Column(name = "base")
+		private String base;
+		
+		@Column(name = "ligamento")
+		private String ligamento;
+		
+		@Column(name = "codigo_acabado")
+		private String codigoAcabado;
+		
+		@Column(name = "ancho_total")
+		private BigDecimal anchoTotal;
+		
+		@Column(name = "peso_gramaje")
+		private BigDecimal pesoGramaje;
+		
+		@Column(name = "peso")
+		private BigDecimal peso;
+		
+		@Column(name = "color")
+		private String color;
+		
+		@Column(name = "composicion")
+		private String composicion;
+		
+		@Column(name = "tipo_tejido")
+		private String tipoTejido;
+		
+		@Column(name = "diseno")
+		private String diseno;
+		
+		@Column(name = "ubicacion")
+		private String ubicacion;
+		
+		@Column(name = "tamano")
+		private String tamano;
+		
+		@Column(name = "tipo_material")
+		private String tipoMaterial;
+		
+		@Column(name = "tipo_prenda")
+		private String tipoPrenda;
+		
+		@Column(name = "maquinas_especiales")
+		private String maquinasEspeciales;
+		
+		@Column(name = "cantidad_prenda")
+		private BigDecimal cantidadPrenda;
+		
+		@Column(name = "objetivo")
+		private String objetivo;
+		
+		@Column(name = "uso_sugerido")
+		private String usoSugerido;
+		
+		@Column(name = "tipo_tintura")
+		private String tipoTintura;
+		
+		@Column(name = "ecologico")
+		private String ecologico;
+		
+		@Column(name = "impacto_social")
+		private String impactoSocial;
+		
+		@Column(name = "norma_certificacion")
+		private String normaCertificacion;
+		
+		@Column(name = "ancho_util")
+		private BigDecimal anchoUtil;
+		
+		@Column(name = "longitud")
+		private BigDecimal longitud;
+		
+		@Column(name = "calibreGrosor")
+		private BigDecimal calibreGrosor;
+		
+		@Column(name = "aseguramiento")
+		private String aseguramiento;
+		
+		@Column(name = "forma")
+		private String forma;
+		
+		@Column(name = "numero_ligas")
+		private BigDecimal numeroLigas;
+		
+		@Column(name = "textura")
+		private String textura;
+		
+		@Column(name = "encogimiento")
+		private String encogimiento;
+		
+		@Column(name = "unidad")
+		private String unidad;
+		
 		@Column(name = "activo")
 		private Boolean activo;
+
+		public Item(Long idItem, String tipoItem, String idTrazabilidad, BigDecimal precioUnidad,
+				BigDecimal cantidadMinima, Boolean activo) {
+			super();
+			this.idItem = idItem;
+			this.tipoItem = tipoItem;
+			this.idTrazabilidad = idTrazabilidad;
+			this.precioUnidad = precioUnidad;
+			this.cantidadMinima = cantidadMinima;
+			this.activo = activo;
+		}
 		
 		
 

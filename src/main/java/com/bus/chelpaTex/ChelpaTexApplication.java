683e691cfc09175207daade48747c1f0daad81f0
Andres9601 <sandresi9601@gmail.com>
2023-02-22 18:19:33 -0500

    Update ChelpaTexApplication.java

--- src/main/java/com/bus/chelpaTex/ChelpaTexApplication.java
+++ src/main/java/com/bus/chelpaTex/ChelpaTexApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.bus.chelpaTex.Entity","com.bus.chelpaTex.Controller","com.bus.chelpaTex.DTO","com.bus.chelpaTex.Repo","com.bus.chelpaTex.Service","com.bus.chelpaTex.ServiceImpl","com.bus.chelpaTex.Security"})
+@ComponentScan(basePackages = {"com.bus.chelpaTex.Entity","com.bus.chelpaTex.Controller","com.bus.chelpaTex.DTO","com.bus.chelpaTex.Repo","com.bus.chelpaTex.Service","com.bus.chelpaTex.ServiceImpl","com.bus.chelpaTex.Security","com.bus.chelpaTex.Utils"})
 public class ChelpaTexApplication {
 
 	public static void main(String[] args) {
